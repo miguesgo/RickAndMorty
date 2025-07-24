0169e8c239b053488c2547fa88565ce60f1cec9a
miguesgo <miguesturtle@outlook.com>
2025-07-23 23:35:34 -0600

    Add project files.

---
+++ Character/Character.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RickAndMorty.Character
+{
+    public class Character
+    {
+        [Key]
+        public required string Id { get; set; }
+
+        public int CharacterId { get; set; }
+
+        public string? Name { get; set; }
+
+        [Display(Name = "Image URL")]
+        public string? ImageUrl { get; set; }
+
+        public string? Species { get; set; }
+
+        public string? Status { get; set; }
+
+        public string? Note { get; set; }
+    }
+}
